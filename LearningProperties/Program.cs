6653803c94e8efd8addc7380589a427ab336e56b
amitksms97 <amitksms97@gmail.com>
2021-05-01 17:33:34 +0530

    Add project files.

---
+++ LearningProperties/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LearningProperties
+{
+    class Player
+    {
+        public bool isAlive = true;
+        int health = 100;
+        
+        public void Hit()
+        {
+            Random r = new Random();
+            health -= r.Next(5, 50);
+        }
+         
+        public void CheckDeath()
+        {
+            if (health <= 0)
+                isAlive = false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            for (int i =0; i< 20; i++)
+            {
+                player.Hit();
+                player.CheckDeath();
+                Console.WriteLine("IS player alive: " + player.isAlive);
+            }
+        }
+    }
+}
